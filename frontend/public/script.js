d69bc3dfad514d065276b541f792b42329be399f
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2022-05-02 21:03:30 +0200

    initial commit

---
+++ frontend/public/script.js
@@ -0,0 +1,32 @@
+
+const headerContent = () => {
+    return `
+    <header>
+        <h1></h1>
+    </header>
+    `
+};
+
+const mainContent = () => {
+    return `
+    <article>
+        <div></div>
+    </article>  
+    `
+};
+
+const pictureContent = () => {
+    return `
+    <img src="">
+    `
+};
+
+const footerContent = () => {
+    return `
+    <footer></footer>
+    `
+};
+
+
+
+window.addEventListener('load', loadEvent);
