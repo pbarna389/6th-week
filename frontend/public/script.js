48952bf754968d4b7baa60dca8c3d11dbef2b3d1
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2022-05-03 13:59:59 +0200

    prototypes added

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -2,7 +2,59 @@
 const headerContent = () => {
     return `
     <header>
-        <h1></h1>
+        <nav>
+            <div> 
+                <img src="">
+            </div>
+            <div>
+                <p>
+                    <span>cruise color splash</span>
+                </p>
+                <div class="dropdown-1">
+                    <span>shoes</span>
+                    <div class="dropdown-content-1">
+                        <ul>
+                            <li>boots</li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <ul>
+                            <li>loafers</li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <ul>
+                            <li>mules</li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <ul>
+                            <li>sandals</li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <ul>
+                            <li>pumps</li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <figure>dollar</figure>
+                <figure>prof</figure>
+                <figure>search</figure>
+                <figure>hearth</figure>
+                <figure>shopcart</figure>
+            </div>
+        </nav>
     </header>
     `
 };
@@ -27,6 +79,10 @@ const footerContent = () => {
     `
 };
 
+const loadEvent = () => {
+    const rootEl = document.querySelector('#root');
 
+    // rootEl.insertAdjacentHTML('beforeend', headerContent())
+}
 
 window.addEventListener('load', loadEvent);
